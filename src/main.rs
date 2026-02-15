55de08190df9aaaf858799121d443b1e98b631e9
Raymond Wong <1049204910@qq.com>
2026-02-16 01:29:12 +0800

    add application icon

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,25 @@ mod app;
 mod ui;
 mod utils;
 
+use iced::window;
+
 fn main() -> iced::Result {
-    iced::run(app::update, app::view)
+    let icon_data = include_bytes!("../assets/icon.png");
+
+    let icon = match image::load_from_memory(icon_data) {
+        Ok(img) => {
+            let rgba = img.to_rgba8();
+            let (width, height) = rgba.dimensions();
+            window::icon::from_rgba(rgba.into_raw(), width, height).ok()
+        }
+        Err(_) => None,
+    };
+
+    iced::application(|| app::DownloadApp::default(), app::update, app::view)
+        .title("Simple MP3 Downloader")
+        .window(window::Settings {
+            icon,
+            ..Default::default()
+        })
+        .run()
 }
